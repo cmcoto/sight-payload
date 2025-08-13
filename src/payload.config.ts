24dbe641ac89348b3ca1e7fcee997c08c841be76
cmcoto <cmcoto@gmail.com>
2025-08-13 22:18:38 +0200

    Working with Uploadthing

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -67,7 +67,7 @@ export default buildConfig({
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URI || '',
-      // authToken: process.env.DATABASE_AUTH_TOKEN || '',
+      authToken: process.env.DATABASE_AUTH_TOKEN || '',
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users],
